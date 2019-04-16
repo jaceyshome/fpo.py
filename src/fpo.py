e74fef7ccd3f32db60416a2931d99d74afaa8e14
Jake Wang <jaceyshome@gmail.com>
2019-04-16 19:57:54 +1000

    init apply

--- src/fpo.py
+++ src/fpo.py
@@ -1,4 +1,4 @@
-    
+
 '''
 ##FPO.ap(...)
 Produces a new list that is a concatenation of sub-lists, each produced by calling FPO.map(..) with each mapper function and the main list.
@@ -7,22 +7,48 @@ Produces a new list that is a concatenation of sub-lists, each produced by calli
     fns:   a list of functions
     list:  a list
 
-###Returns:    
+###Returns:
     a list of new values
 
 ###Example:
     fn1 = lambda increment(v) { return v + 1 }
     def double(v) { return v * 2 }
     nums = [1,2,3,4,5]
-    
+
 ###Reference:
     # https://treyhunner.com/2015/12/python-list-comprehensions-now-in-color/
 '''
-
-
 def ap(fns, list):
     matrix = [[fn(v) for v in list] for fn in fns]
     return [n for row in matrix for n in row]
+
+
+
+'''
+##FPO.apply(...)
+Wraps a function to spread out the properties from an object arugment as individual positional arguments
+
+###Args:
+    fn: function to wrap
+    props: (optional) list of property names (strings) to indicate the order to spread properties as individual arguments. If omitted, the signature of fn is parsed for its parameter list to try to determine an ordered property list; this detection only works for simple parameters (including those with default parameter value settings).
+
+
+###Returns:
+    function
+
+###Example:
+    def foo(x,y = 2): return x + y
+    def bar([a,b],c): return a + b + c
+
+    f = FPO.apply(fn: foo)
+    p = FPO.apply(fn: bar, props:["x","y"]);
+
+    f({x: 1, y:1})   // 2
+    f({x: 3})         // 5
+    p({x:[1,2], y: 3}))    // 6
+'''
+def apply(fn, props):
+    pass
 
 
 
@@ -34,7 +60,7 @@ Plucks properties form the given list and return a list of properties' values
     list:   list
     *args:  properties
 
-###Returns:    
+###Returns:
     a list of values
 
 ###Example:
